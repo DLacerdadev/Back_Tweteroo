6df7aea55a367bcf61fd4ec9b73655333d009076
Daniel Lacerda <dlacerda.dev@gmail.com>
2024-03-19 00:28:52 -0300

    cadastro e leitura de tweets

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 5000;
 
 let users = [];
+let tweets = [];
 
 app.use(bodyParser.json());
 
@@ -20,6 +21,32 @@ app.post("/sign-up", (req, res) => {
   users.push(newUser);
 
   res.status(201).send("Usuário cadastrado com sucesso");
+});
+
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+
+  const existingUser = users.find((user) => user.username === username);
+  if (!existingUser) {
+    return res.status(400).send("Usuário não encontrado");
+  }
+
+  const newTweet = { username, avatar: existingUser.avatar, tweet };
+  tweets.push(newTweet);
+
+  res.status(201).send("Tweet enviado com sucesso");
+});
+
+app.get("/tweets", (req, res) => {
+  const lastTenTweets = tweets.slice(Math.max(tweets.length - 10, 0));
+
+  const tweetsWithAvatar = lastTenTweets.map((tweet) => {
+    const { username, tweet: tweetContent } = tweet;
+    const user = users.find((user) => user.username === username);
+    return { username, avatar: user.avatar, tweet: tweetContent };
+  });
+
+  res.json(tweetsWithAvatar);
 });
 
 app.listen(port, () => {
